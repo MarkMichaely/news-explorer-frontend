0b434fd4a92fa414e716e8b88038093e3b875fd7
mark <markmichaely@gmail.com>
2023-05-06 01:24:51 +0300

    added footer and styling

---
+++ src/components/App/App.js
@@ -0,0 +1,28 @@
+
+import './App.css';
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import Header from '../Header/Header';
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+import SavedNews from '../SavedNews/SavedNews'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path={'/'}>
+          <Main />
+        </Route>
+        <Route path={'/saved-news'}>
+          <SavedNews />
+        </Route>
+      </Switch>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import './Footer.css'
+import { Link } from "react-router-dom";
+import facebookIcon from "../../images/facebookIcon.svg";
+import githubIcon from "../../images/githubIcon.svg";
+
+const year = new Date().getFullYear();
+function Footer() {
+    return (
+        <footer className="footer">
+            <p className="footer__copyright">{`© ${year} Supersite, Powered by News API`}</p>
+            <div className='footer__container'>
+                <div className="footer__links">
+                    <Link className="footer__link" to="/">
+                        Home
+                    </Link>
+                    <a className="footer__link" href="https://practicum.com">
+                        Practicum
+                    </a>
+                </div>
+                <div className='footer__social'>
+                    <a href="https://github.com/MarkMichaely/" class="footer__social-link"
+                    ><img
+                            src={githubIcon}
+                            alt="github-icon"
+                            class="footer__social-icon"
+                        />
+                    </a>
+                    <a href="https://facebook.com" class="footer__social-link"
+                    ><img
+                            src={facebookIcon}
+                            alt="facebook-icon"
+                            class="footer__social-icon"
+                        />
+                    </a>
+                </div>
+            </div>
+        </footer>)
+}
+export default Footer;
